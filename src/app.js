68d013b16dbb2ce655e31c9f0e29845a22fef8df
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2024-02-11 03:46:07 +0300

    Add root level express rate limit (#7)

--- src/app.js
+++ src/app.js
@@ -2,6 +2,7 @@ import express from 'express'
 
 import dummyRouter from 'routers'
 import errorHandler from 'middlewares/error-handler'
+import rateLimiter from './middlewares/rate-limiter'
 
 const healthz = (app) => {
   app.get('/healthz', (req, res) => {
@@ -13,6 +14,7 @@ const buildApp = (additionalRoutesCallback = (app) => {}) => {
   const app = express()
 
   healthz(app)
+  app.use(rateLimiter)
   app.use('/dummy', dummyRouter)
   app.use(errorHandler)
   additionalRoutesCallback?.(app)
---
+++ src/middlewares/rate-limiter.js
@@ -0,0 +1,9 @@
+import rateLimit from 'express-rate-limit'
+
+const rateLimiter = rateLimit({
+  windowMs: 1 * 60 * 1000, // 1 minute
+  max: 1000, // 1000 requests
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+})
+
+export default rateLimiter
