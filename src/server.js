789fab16cffc0365d72fbd60c1b017b046a7c2d1
Samed Kahyaoglu <32146055+urtuba@users.noreply.github.com>
2025-03-20 15:24:24 +0300

    Reconsider responsibility of husky hooks

--- src/server.js
+++ src/server.js
@@ -1,5 +1,6 @@
-import registerDefaultMiddlewares from 'middlewares/default'
 import HTTP from 'node:http'
+
+import registerDefaultMiddlewares from 'middlewares/default'
 import ENV from 'utils/environment'
 
 import buildApp from './app'
@@ -9,6 +10,6 @@ const app = buildApp(registerDefaultMiddlewares)
 const srv = HTTP.createServer(app)
 
 srv.listen(ENV.PORT, () => {
-  // biome-ignore lint/style/noConsole: <explanation>
+  // biome-ignore lint/suspicious/noConsole: <explanation>
   console.log(`Server is running on port ${ENV.PORT}`)
 })
